1d4736ccc228b7eac424128573efbc42f7668d0b
tycook <tycook1@Tylers-MacBook-Pro.local>
2016-11-29 20:07:55 -0700

    Added new GUI with fixed variable names

---
+++ ResumeBuilder/src/resumebuilder/ResumeBuilder.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package resumebuilder;
+
+import javax.swing.JScrollPane;
+
+/**
+ *
+ * @author tycook1
+ */
+public class ResumeBuilder {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        HomeScreen m = new HomeScreen();
+        m.setVisible(true);
+    }
+    
+}
