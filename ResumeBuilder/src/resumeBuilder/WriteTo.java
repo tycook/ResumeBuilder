0e92e3ad0c4cb951674a77547d4a461cff210275
David Pasquale <davpasquale@gmail.com>
2016-12-01 16:10:46 -0700

    Added more functionality, now includes bullets.

--- ResumeBuilder/src/resumeBuilder/WriteTo.java
+++ ResumeBuilder/src/resumeBuilder/WriteTo.java
@@ -1,7 +1,9 @@
-package resumeBuilder;
+package resumebuilder;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.math.BigInteger;
+
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.apache.poi.xwpf.usermodel.XWPFParagraph;
 import org.apache.poi.xwpf.usermodel.XWPFRun;
@@ -22,6 +24,19 @@ public class WriteTo {
 	    return run;
 		}
 	   
+	   public static XWPFRun createBullet(XWPFParagraph paragraph, String text){
+		XWPFRun run = paragraph.createRun();
+		    run.setFontFamily("Wingdings");
+		    run.setBold(true);
+		    run.setFontSize(8);
+		    run.setText(text);
+		    paragraph.setSpacingBefore(0);
+		    paragraph.setSpacingAfter(0);
+		    return run;
+			}
+	   
+	  
+	   
 	   public static void main(String[] args)throws Exception 
 	   {
 	   //Create Document Document
@@ -40,11 +55,43 @@ public class WriteTo {
 	   email.setAlignment(ParagraphAlignment.CENTER);
 	   
 	   XWPFParagraph phone = document.createParagraph();
-	   XWPFRun phonerun = createRun(phone, 12, false, "720-555-5555");
+	   XWPFRun phonerun = createRun(phone, 12, false, "720-555-5555");	   
 	   phone.setAlignment(ParagraphAlignment.CENTER);
 	   
+	   XWPFParagraph foo = document.createParagraph();
 	   
+	   XWPFParagraph edu = document.createParagraph();
+	   XWPFRun runedu = createRun(edu, 16, true, "Education");
+	   edu.setAlignment(ParagraphAlignment.LEFT);
 	   
+	   XWPFParagraph education = document.createParagraph();
+	   XWPFRun bullet1 = createBullet(education, "      l ");
+	   XWPFRun university = createRun(education, 12, false, "Attended " + "University of Colorado Boulder " + "and graduated on " + "11/1993.");
+	   
+	   XWPFParagraph major = document.createParagraph();
+	   XWPFRun bullet2 = createBullet(major, "      l ");
+	   XWPFRun majorrun = createRun(major, 12, false, "Majored in " + "Computer Science");
+	   
+	   XWPFParagraph foo1 = document.createParagraph();
+	   XWPFParagraph exp = document.createParagraph();
+	   XWPFRun exprun = createRun(exp, 16, true, "Work Experience");
+
+	   
+	   XWPFParagraph workexp = document.createParagraph();
+	   XWPFRun bullet3 = createBullet(workexp, "      l ");
+	   XWPFRun workexprun = createRun(workexp, 12, false, "Worked for " + "Company " + "from " + "12/1993 " + "to " + "1/1994");
+	   
+	   XWPFParagraph jobDesc = document.createParagraph();
+	   XWPFRun bullet4 = createBullet(jobDesc, "      l ");
+	   XWPFRun jobDescRun = createRun(jobDesc, 12, false, "Cleaned toilets, balanced checkbooks, and bathed grandmas");
+	   
+	   XWPFParagraph foo2 = document.createParagraph();
+	   XWPFParagraph ref = document.createParagraph();
+	   XWPFRun refrun = createRun(ref, 16, true, "References");
+
+	   XWPFParagraph reference1 = document.createParagraph();
+	   XWPFRun bullet5 = createBullet(reference1, "      l ");
+	   XWPFRun ref1run = createRun(reference1, 12, false, "John Snow 999-999-9999");
 	   
 	   
 	   document.write(out);
