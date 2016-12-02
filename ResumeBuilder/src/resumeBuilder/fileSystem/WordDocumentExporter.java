689037f2c221956fc7e3449517f584b5ca25ceb9
derekriemer <derek.riemer@colorado.edu>
2016-12-01 18:32:46 -0700

    Add word document exporter

---
+++ ResumeBuilder/src/resumeBuilder/fileSystem/WordDocumentExporter.java
@@ -0,0 +1,21 @@
+package resumeBuilder.fileSystem;
+
+import java.util.Iterator;
+
+import resumeBuilder.storage.sections.MajorSection;
+
+public class WordDocumentExporter implements Saver {
+
+	@Override
+	public void initialize(String resumeName) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void write(Iterator<MajorSection> sections) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
