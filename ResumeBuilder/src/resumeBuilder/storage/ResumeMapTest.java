98d8775c34e926435fd06c1caaed08243a6ebc01
derekriemer <derek.riemer@colorado.edu>
2016-12-04 18:31:05 -0700

    Basic tests for resume map

---
+++ ResumeBuilder/src/resumeBuilder/storage/ResumeMapTest.java
@@ -0,0 +1,49 @@
+package resumeBuilder.storage;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ResumeMapTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testGetResumeNames() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLoad() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAdd() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGet() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testContains() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSave() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemove() {
+		fail("Not yet implemented");
+	}
+
+}
